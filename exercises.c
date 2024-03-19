ff94d70b8c345042d1e5066e4f2f6abcaff8ccf7
Your Name <you@example.com>
2024-03-19 16:46:16 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -131,7 +131,7 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 
 int parentesisBalanceados(char *cadena) 
 {
-  /*int i, j;
+  int i, j;
 
   int largo = strlen(cadena);
 
@@ -152,49 +152,4 @@ int parentesisBalanceados(char *cadena)
       
     }
   }
-  return 1;*/
-  
-  Stack* P1 = create_stack();
-  Stack* P2 = create_stack();
-  int i, j;
-  int largo = strlen(cadena);
-  if (largo % 2 == 1) return 0;
-  for (i = 0, j = largo - 1; i < j; i++, j--)
-    {
-      switch (cadena[i])
-        {
-          case '(':
-            pushFront(P1, &cadena[i]);
-            break;
-          case '{':
-            {
-              pushFront(P1, &cadena[i]);
-              break;
-            }
-          case '[':
-            {
-              pushFront(P1, &cadena[i]);
-              break;
-            }
-          case ')':
-            {
-              if(cadena[j] != '(') return 0;
-              popFront(P1);
-              break;
-            }
-          case '}':
-            {
-              if(cadena[j] != '{') return 0;
-              popFront(P1);
-              break;
-            }
-          case ']':
-            {
-              if(cadena[j] != '[') return 0;
-              popFront(P1);
-              break;
-            }
-        }
-    }
-  return 1;
 }
